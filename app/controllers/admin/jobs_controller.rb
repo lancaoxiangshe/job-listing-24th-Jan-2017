19d2e531688d90663b15ce9c5f9e307326bdc5d7
lancaoxiangshe <wulantoo@live.com>
2017-01-08 22:51:37 +0800

    implement part4

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -28,6 +28,7 @@ class Admin::JobsController < ApplicationController
     @job = Job.find(params[:id])
     if @job.update(job_params)
       redirect_to admin_jobs_path
+    else
     render :edit
   end
 end
