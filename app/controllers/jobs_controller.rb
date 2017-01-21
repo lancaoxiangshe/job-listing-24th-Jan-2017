a11fc188817a348767de0d019c187bc690fd3c32
lancaoxiangshe <wulantoo@live.com>
2017-01-21 16:45:54 +0800

    modify the job listing page

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,7 +1,9 @@
 class JobsController < ApplicationController
   before_filter :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :validate_search_key, only: [:search]
 
   def index
+
     @jobs = case params[:order]
             when 'by_lower_bound'
               Job.published.order("wage_lower_bound DESC")
@@ -52,6 +54,22 @@ class JobsController < ApplicationController
     @job.destroy
     redirect_to jobs_path
   end
+
+  def search
+      if @query_string.present?
+        search_result = Job.ransack(@search_criteria).result(:distinct => true)
+        @jobs = search_result.paginate(:page => params[:page], :per_page => 20 )
+      end
+    end
+
+
+    protected
+
+    def validate_search_key
+      @query_string = params[:q].gsub(/\\|\'|\/|\?/, "") if params[:q].present?
+      @search_criteria = search_criteria(@query_string)
+    end
+
 
 private
   def job_params
